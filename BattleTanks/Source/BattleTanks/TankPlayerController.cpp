91465b6a379baeafa4a3fdc6cf2cd87d64026fcb
DESKTOP-9F1FLU7\ahmed <ahmedgebril1889@yahoo.com>
2018-07-02 03:01:51 -0700

    Changing some Project Settings

---
+++ BattleTanks/Source/BattleTanks/TankPlayerController.cpp
@@ -0,0 +1,11 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+#include "TankPlayerController.h"
+#include "public/Tank.h"
+#include "TankPlayerController.h"
+
+
+ATank*ATankPlayerController::GetControlledTank()const {
+
+	return Cast<ATank>(GetPawn());
+}
+
